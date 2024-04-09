67aa60c9ac9db9dbf498b1f8786d24727726c883
adityaaaaaaaa1407 <adityasagavekar14@gmail.com>
2024-04-09 12:46:01 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <div>
+      <p className="text-3xl font-bold text-red-700">hello aditya</p>
+    </div>
+    </>
+  );
+}
